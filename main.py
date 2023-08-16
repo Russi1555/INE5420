0b989c732c45d3ccfa884d567ccd8aa52f17b3d1
Russi1555 <78427180+Russi1555@users.noreply.github.com>
2023-08-16 20:10:07 -0300

    Translação
    Sem poligonos ainda

--- main.py
+++ main.py
@@ -1,67 +1,91 @@
-from PyQt5 import QtGui, QtWidgets, QtCore
+from PyQt5 import QtGui
+from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTimer
 from wireframe import wireframe
 import re
 import sys
 
-report = True
-
 class MainWindow(QMainWindow):
-    """
-    Janela principal do programa.
-    """
     def __init__(self):
-        """
-        Construtor, seta os elementos principais da janela.
-        """
         super().__init__()
         self.title = "Trabalho 1.1"
-        self.setWindowTitle(self.title)
-
         self.top= 150
         self.left= 150
-        self.window_width = 1200
-        self.window_height = 800
-        self.setGeometry(self.top, self.left, self.window_width, self.window_height)
-        
         self.objetos = {}
+        self.x_translacao = 0
+        self.y_translacao = 0
+        self.rotacao = 0
+        self.width = 1200
+        self.height = 800
+        self.InitWindow()
+        
+
+    def InitWindow(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.top, self.left, self.width, self.height)
         self.setStyleSheet("background-color: grey")
         self.UiComponents()
         self.show()
     
+    def incrementa_translacao_horizontal(self, value):
+            self.x_translacao += value
+            self.update()
+            
+    def incrementa_translacao_vertical(self, value):
+            self.y_translacao += value
+            self.update()
+
     def UiComponents(self):
-        """
-        Inicia os componentes padroes da janela principal.
-        """
+
+
         self.viewport = QtWidgets.QLabel()
         self.viewport.setGeometry(QtCore.QRect(200,10,990,600))
 
-        # Cria o botar que gera novos objetos
-        def novo_objeto():
-            """
-            Funcao de leitura do botao de novo objeto.
-            """
-            global report
-            nome, _ = QtWidgets.QInputDialog.getText(self, 'Nome do poligono','Digite um nome para o poligono: ')
-            coords, _ = QtWidgets.QInputDialog.getText(self, 'Coordenadas do poligono','Digite as coordenadas do poligono: ')
-            # Transform number pairs into a list of lists
-            coords = [(int(x), int(y)) for x, y in re.findall(r'\((\d+),(\d+)\)', coords)]
-            
-            if report: print(nome,coords)
-            self.objetos[nome] = (wireframe(nome,coords))
-            if report: print(self.objetos[nome])
-            self.update()
+        b_novo_objeto = QPushButton("Novo Objeto", self)
+        b_novo_objeto.setGeometry(50,50,100,100)
+        b_novo_objeto.setStyleSheet("background-color: white")
+        b_novo_objeto.clicked.connect(self.novo_objeto)
+
+        b_translacao_cima = QPushButton("↑", self)
+        b_translacao_cima.setGeometry(75,185,30,30)
+        b_translacao_cima.setStyleSheet("background-color: white")
+        b_translacao_cima.clicked.connect(lambda: self.incrementa_translacao_vertical(-10))
         
-        botao_novo_objeto = QPushButton("Novo Objeto", self)
-        botao_novo_objeto.setGeometry(50,50,100,100)
-        botao_novo_objeto.setStyleSheet("background-color: white")
-        botao_novo_objeto.clicked.connect(novo_objeto)
+        
+        b_translacao_esquerda = QPushButton("←",self)
+        b_translacao_esquerda.setGeometry(45,200,30,30)
+        b_translacao_esquerda.setStyleSheet("background-color: white")
+        b_translacao_esquerda.clicked.connect(lambda: self.incrementa_translacao_horizontal(-10))
+
+        b_translacao_direita = QPushButton("→",self)
+        b_translacao_direita.setGeometry(105,200,30,30)
+        b_translacao_direita.setStyleSheet("background-color: white")
+        b_translacao_direita.clicked.connect(lambda: self.incrementa_translacao_horizontal(10))
+
+        print(self.x_translacao)
+        b_translacao_baixo = QPushButton("↓",self)
+        b_translacao_baixo.setGeometry(75,215,30,30)
+        b_translacao_baixo.setStyleSheet("background-color: white")
+        b_translacao_baixo.clicked.connect(lambda: self.incrementa_translacao_vertical(10))
+
+    def novo_objeto(self):
+        nome,coords = self.take_inputs()
+        print(nome,coords)
+        self.objetos[nome] = (wireframe(nome,coords))
+        print(self.objetos[nome])
+
+
+
+    def take_inputs(self):
+        nome,inserido1 = QtWidgets.QInputDialog.getText(self, 'Nome do poligono','Digite um nome para o poligono: ')
+        coords,inserido2 = QtWidgets.QInputDialog.getText(self, 'Coordenadas do poligono','Digite as coordenadas do poligono: ')
+        number_pairs = re.findall(r'\((\d+),(\d+)\)', coords)
+        # Transform number pairs into a list of lists
+        coords = [[int(x), int(y)] for x, y in number_pairs]
+        return([nome, coords])
 
     def paintEvent(self, event):
-        """
-        Eu nao tenho certeza de quando exatamente isso aqui eh chamado.
-        """
         qp = QtGui.QPainter()
         qp.begin(self)
         qp.setPen(QtGui.QPen(Qt.green, 8))
@@ -69,12 +93,12 @@ class MainWindow(QMainWindow):
         qp.setPen(QtGui.QPen(Qt.red,4))
         for nome in self.objetos:
             #é bunda mas vou fazer a transformação de viewport sempre que for desenhar por enquanto só pra testar
-            x1, y1 = self.objetos[nome].coords[0][0], self.objetos[nome].coords[0][1]
-            x2, y2 = self.objetos[nome].coords[1][0],self.objetos[nome].coords[1][1]
-            xv1 = self.viewport.x() + (x1  * (self.viewport.width()/self.window_width))
-            yv1 = self.viewport.y() + (y1 * (self.viewport.height()/self.window_height))
-            xv2 = self.viewport.x() + (x2 * (self.viewport.width()/self.window_width))
-            yv2 = self.viewport.y() + (y2 * (self.viewport.height()/self.window_height))
+            x1, y1 = self.objetos[nome].coords[0][0] + self.x_translacao, self.objetos[nome].coords[0][1] + self.y_translacao
+            x2, y2 = self.objetos[nome].coords[1][0] + self.x_translacao,self.objetos[nome].coords[1][1] + self.y_translacao
+            xv1 = self.viewport.x() + (x1  * (self.viewport.width()/self.width))
+            yv1 = self.viewport.y() + (y1 * (self.viewport.height()/self.height))
+            xv2 = self.viewport.x() + (x2 * (self.viewport.width()/self.width))
+            yv2 = self.viewport.y() + (y2 * (self.viewport.height()/self.height))
 
 
             print(xv1,yv1, xv2, yv2)
@@ -132,6 +156,8 @@ class Registro_Novo_Obj(QtWidgets.QMainWindow):
     def PrintInput(self):
         print ([self.nome.text(),self.coords.text()])
         self.close()
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
