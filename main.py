523d05325436454436d2d2a491d653f49afe77b8
Russi1555 <russi.gustavo00@gmail.com>
2023-08-18 14:17:11 -0300

    Interface um pouco mais bonita

--- main.py
+++ main.py
@@ -6,10 +6,71 @@ import re
 import sys
 from collections.abc import Callable
 
+class WindowInput(QMainWindow):    
+    '''
+    Janela secundário para input do novo objeto renderizavel
+    '''
+    submitClicked = QtCore.pyqtSignal(tuple) #Sinal para transferência de dados entre janelas do PyQt
+    def __init__(self, parent = None):
+        super().__init__()
+        super().__init__(parent)
+        self.nome = ""
+        self.coords = ""
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.PrintInput)
+
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(300, 130)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(20, 30, 71, 16))
+        self.label.setObjectName("label")
+
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(20, 60, 71, 16))
+        self.label_2.setObjectName("label_2")
+
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(20, 80, 300, 16))
+        self.label_3.setText("obs: Coordenadas em formato (x1,y1) (x2,y2)...")
+
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(20, 100, 251, 23))
+        self.pushButton.setObjectName("pushButton")
+
+        self.nome = QtWidgets.QLineEdit(self.centralwidget)
+        self.nome.setGeometry(QtCore.QRect(100, 30, 171, 20))
+        self.nome.setObjectName("Nome")
+
+        self.coords = QtWidgets.QLineEdit(self.centralwidget)
+        self.coords.setGeometry(QtCore.QRect(100, 60, 171, 20))
+        self.coords.setObjectName("Coordenadas")       
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle("Novo Objeto")
+        self.label.setText("Nome")
+        self.label_2.setText("Coordenadas")
+        self.pushButton.setText("Criar")
+
+    def PrintInput(self):
+        '''
+        emite os valores introduzidos nas caixas de texto para serem recebidos pela janela principal
+        '''
+        print ([self.nome.text(),self.coords.text()])
+        self.submitClicked.emit((self.nome.text(), self.coords.text()))
+        self.close()
+
 class MainWindow(QMainWindow):
     """
     Janela principal do programa.
-    """
+    """ 
     def __init__(self):
         """
         Construtor, seta os elementos principais da janela.
@@ -25,7 +86,7 @@ class MainWindow(QMainWindow):
         self.setGeometry(self.top, self.left, self.window_width, self.window_height)
         
         self.objetos = {}
-        self.setStyleSheet("background-color: grey")
+        self.setStyleSheet("background-color: light grey")
         self.UiComponents()
         self.show()
     
@@ -70,16 +131,18 @@ class MainWindow(QMainWindow):
         def novo_objeto():
             """
             Funcao de leitura do botao de novo objeto.
+            Invoca a janela de input.
             """
             global report
-            nome, _ = QtWidgets.QInputDialog.getText(self, 'Nome do poligono','Digite um nome para o poligono: ')
-            coords, _ = QtWidgets.QInputDialog.getText(self, 'Coordenadas do poligono','Digite as coordenadas do poligono: ')
+            self.WindowInput()
+            #nome, _ = QtWidgets.QInputDialog.getText(self, 'Nome do poligono','Digite um nome para o poligono: ')
+            #coords, _ = QtWidgets.QInputDialog.getText(self, 'Coordenadas do poligono','Digite as coordenadas do poligono: ')
             # Transform number pairs into a list of lists
-            coords = [(int(x), int(y)) for x, y in re.findall(r'\((\d+),(\d+)\)', coords)]
+           # coords = [(int(x), int(y)) for x, y in re.findall(r'\((\d+),(\d+)\)', coords)]
             
-            self.objetos[nome]: wireframe = wireframe(nome,coords, False)
-            self.objetos[nome].update_viewport(self.viewport.x(), self.viewport.y(), self.viewport.width(), self.viewport.height(), self.window_width, self.window_height)
-            self.update()
+            #self.objetos[nome]: wireframe = wireframe(nome,coords, False)
+            #self.objetos[nome].update_viewport(self.viewport.x(), self.viewport.y(), self.viewport.width(), self.viewport.height(), self.window_width, self.window_height)
+            #self.update()
         
         # Botao de novo objeto
         self.New_button("Novo Objeto", 50,50,100,100, novo_objeto)
@@ -96,6 +159,23 @@ class MainWindow(QMainWindow):
 
         # Botao de giro
         self.New_button("O", 75,320,30,30, self.girar, (30))
+    
+    def WindowInput(self):                                             # <===
+        self.w = WindowInput()
+        self.w.submitClicked.connect(self.instanciarNovoObjeto) #Quando recebe o sinal submitClicked, passa a mensagem como parametro para InstanciarNovoObjeto
+        self.w.show()
+
+    def instanciarNovoObjeto(self, pacote_n_c):
+        nome = pacote_n_c[0]
+        coords =  pacote_n_c[1]
+        if nome =="" or coords == "":
+            print("VALORES INVALIDOS")
+        else:
+            coords = [(int(x), int(y)) for x, y in re.findall(r'\((\d+),(\d+)\)', coords)]
+                
+            self.objetos[nome]: wireframe = wireframe(nome,coords, False)
+            self.objetos[nome].update_viewport(self.viewport.x(), self.viewport.y(), self.viewport.width(), self.viewport.height(), self.window_width, self.window_height)
+            self.update()
 
     def incrementa_translacao_horizontal(self, value: int):
         """
@@ -148,7 +228,8 @@ class MainWindow(QMainWindow):
         qp = QtGui.QPainter()
         qp.begin(self)
         # Desenha o retangulo verde da viewport
-        qp.setPen(QtGui.QPen(Qt.green, 1))
+        qp.setPen(QtGui.QPen(Qt.black, 1))
+        qp.setBrush(QtGui.QBrush(Qt.white, Qt.SolidPattern))
         qp.drawRect(self.viewport.x(),self.viewport.y(),self.viewport.width(),self.viewport.height())
         
         # Itera sobre os wireframes
