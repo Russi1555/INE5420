57a870f9118c9716167e2fb18b66c95e09aeadf8
Russi1555 <russi.gustavo00@gmail.com>
2023-08-22 15:03:01 -0300

    Coisa linda UI linda

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from PyQt5 import QtGui, QtWidgets, QtCore
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QCheckBox
 from PyQt5.QtCore import Qt
 from wireframe import wireframe
 import re
@@ -67,6 +67,18 @@ class WindowInput(QMainWindow):
         self.submitClicked.emit((self.nome.text(), self.coords.text()))
         self.close()
 
+class ListWidget(QtWidgets.QListWidget, QMainWindow):
+    def __init__(self, mainwindow):
+        super().__init__(mainwindow)
+        self.mainwindow = mainwindow
+
+    def clicked(self, item):
+        print(str(item.text()))
+        self.mainwindow.objetos[str(item.text())].selecionado = not self.mainwindow.objetos[str(item.text())].selecionado
+        self.mainwindow.update()
+        print(self.mainwindow.objetos[str(item.text())].selecionado)
+        #return str(item.text())
+
 class MainWindow(QMainWindow):
     """
     Janela principal do programa.
@@ -84,12 +96,28 @@ class MainWindow(QMainWindow):
         self.window_width = 1200
         self.window_height = 800
         self.setGeometry(self.top, self.left, self.window_width, self.window_height)
+        #transformation quantities
+        self.tqt: float = 0 #translation
+        self.sqt: float = 0 #stretch
+        self.rqt: float = 0 #rotation
+
+        #center point for rotation
+        self.center_x = None
+        self.center_y = None
         
         self.objetos = {}
         self.setStyleSheet("background-color: light grey")
         self.UiComponents()
         self.show()
     
+    @property
+    def cx(self):
+        return None if self.center_x.text() == "" else float(self.center_x.text())
+    
+    @property
+    def cy(self):
+        return None if self.center_y.text() == "" else float(self.center_y.text())
+
     def New_button(self, label: str, x: int, y: int, w: int, h: int, func: Callable, args: list or None = None) -> QPushButton:
         """
         Cria um novo botao
@@ -143,25 +171,56 @@ class MainWindow(QMainWindow):
             #self.objetos[nome]: wireframe = wireframe(nome,coords, False)
             #self.objetos[nome].update_viewport(self.viewport.x(), self.viewport.y(), self.viewport.width(), self.viewport.height(), self.window_width, self.window_height)
             #self.update()
+        
+        def set_selecionado(nome):
+            self.objetos[nome].selecionado = not self.objetos[nome].selecionado
 
-        self.lista_objetos = QtWidgets.QListWidget(self)
+        self.lista_objetos = ListWidget(self)
         self.lista_objetos.setGeometry(10,55,180,200)
+        x = self.lista_objetos.itemClicked.connect(self.lista_objetos.clicked)
+        print("HAHAHA " + str(x))
+        #print(self.objetos[self.lista_objetos.item.text()].selecionado)
         
         # Botao de novo objeto
         self.New_button("Novo Objeto", 50,20,100,30, novo_objeto)
 
         # Botoes direcionais
-        self.New_button("↑", 75,185,30,30, self.incrementa_translacao_vertical, (-10))
-        self.New_button("←", 45,200,30,30, self.incrementa_translacao_horizontal, (-10))
-        self.New_button("→", 105,200,30,30, self.incrementa_translacao_horizontal, (10))
-        self.New_button("↓", 75,215,30,30, self.incrementa_translacao_vertical, (10))
+        atx,aty = 45,300
+        self.tqt = QtWidgets.QLineEdit(self) #quantidade pra mexer
+        self.tqt.setGeometry(atx + 100, aty + 15, 30,30)
+        
+        self.New_button("↑", atx+30,aty,30,30, self.incrementa_translacao, ("cim",))
+        self.New_button("←", atx,aty+15,30,30, self.incrementa_translacao, ("esq",))
+        self.New_button("→", atx+60,aty+15,30,30, self.incrementa_translacao, ("dir",))
+        self.New_button("↓", atx+30,aty+30,30,30, self.incrementa_translacao, ("bax",))
 
-        # Botao de etica e encolhe
-        self.New_button(">", 55,260,30,30, self.estica, (1.5))
-        self.New_button("<", 95,260,30,30, self.estica, (1/1.5))
+        # Botao de estica e encolhe
+        self.sqt = QtWidgets.QLineEdit(self)
+        self.sqt.setGeometry(atx + 100, aty + 75, 30,30)
+        self.New_button(">", atx+10,aty+75,30,30, self.estica, (1))
+        self.New_button("<", atx+50,aty+75,30,30, self.estica, (-1))
 
         # Botao de giro
-        self.New_button("O", 75,320,30,30, self.girar, (30))
+        self.rqt = QtWidgets.QLineEdit(self)
+        self.rqt.setGeometry(atx + 100, aty + 120, 30,30)
+        self.New_button("O", atx+30,aty+120,30,30, self.girar, ())
+
+        # Centro de transformação
+        label_center_x = QtWidgets.QLabel(self)
+        label_center_x.setGeometry(atx - 15, aty +185, 15, 10)
+        label_center_x.setText("X: ")
+        self.center_x = QtWidgets.QLineEdit(self)
+        self.center_x.setGeometry(atx, aty + 175, 30,30)
+
+        label_center_y = QtWidgets.QLabel(self)
+        label_center_y.setGeometry(atx +45, aty +185, 15, 10)
+        label_center_y.setText("Y: ")
+        self.center_y = QtWidgets.QLineEdit(self)
+        self.center_y.setGeometry(atx + 60, aty + 175, 30,30)
+
+        self.render_center_point = QCheckBox(self)
+        self.render_center_point.setGeometry(atx + 105, aty + 180, 15,15)
+ 
     
     def WindowInput(self):                                             # <===
         self.w = WindowInput()
@@ -180,30 +239,24 @@ class MainWindow(QMainWindow):
             self.objetos[nome]: wireframe = wireframe(nome,coords, False)
             self.objetos[nome].update_viewport(self.viewport.x(), self.viewport.y(), self.viewport.width(), self.viewport.height(), self.window_width, self.window_height)
             self.update()
-
-    def incrementa_translacao_horizontal(self, value: int):
-        """
-        Move todos os objetos na tela horizontalmente.
-
-        Args:
-            value (int): deslocamento.
-        """
-        for objeto in self.objetos.values():
-            objeto.translade(value, 0)
-        self.update()
             
-    def incrementa_translacao_vertical(self, value: int):
+    def incrementa_translacao(self, dir: str):
         """
         Move todos os objetos na tela horizontalmente.
 
         Args:
             value (int): delocamento.
         """
+        tqt = 0 if self.tqt.text() == '' else float(self.tqt.text())
+        print(tqt)
+
+        args = (tqt if dir == "dir" else -tqt if dir == "esq" else 0, tqt if dir == "bax" else -tqt if dir == "cim" else 0)
+
         for objeto in self.objetos.values():
-            objeto.translade(0, value)
+            objeto.translade(*args)
         self.update()
 
-    def estica(self, value: int, center: tuple):
+    def estica(self, tipo: int):
         """
         Estica todos os objetos ao redor do proprio centro.
 
@@ -211,11 +264,14 @@ class MainWindow(QMainWindow):
             value (int): Fator de estique.
             center (tuple): Centro da transformacao.
         """
+        value = 1 if self.sqt.text() == '' else float(self.sqt.text()) ** tipo
+        center = None if self.cx is None or self.cy is None else (self.cx, self.cy)
         for objeto in self.objetos.values():
+
             objeto.stretch(value, value, center)
         self.update()
     
-    def girar(self, angle: int, center: tuple):
+    def girar(self):
         """
         Gira todos os objetos ao redor do proprio centro.
 
@@ -223,6 +279,8 @@ class MainWindow(QMainWindow):
             value (int): Angulo.
             center(tuple): Ponto Cental da rotacao.
         """
+        angle = 0 if self.rqt.text() == '' else -float(self.rqt.text())
+        center = None if self.cx is None or self.cy is None else (self.cx, self.cy)
         for objeto in self.objetos.values():
             objeto.rotate(angle, center)
         self.update()
@@ -235,9 +293,16 @@ class MainWindow(QMainWindow):
         qp.setBrush(QtGui.QBrush(Qt.white, Qt.SolidPattern))
         qp.drawRect(self.viewport.x(),self.viewport.y(),self.viewport.width(),self.viewport.height())
         
+        if int(self.render_center_point.checkState()) == 2 and not self.cx is None and not self.cy is None:
+            qp.setPen(QtGui.QPen(Qt.black, 4))
+            qp.drawPoint(QtCore.QPointF(float(self.cx)+self.viewport.x(),float(self.cy)+self.viewport.y()))
+            self.update()
         # Itera sobre os wireframes
         for nome, objeto in self.objetos.items():
-            qp.setPen(QtGui.QPen(Qt.red,4))
+            if self.objetos[nome].selecionado:
+                qp.setPen(QtGui.QPen(Qt.red,4))
+            else:
+                qp.setPen(QtGui.QPen(Qt.red,1))
             last_point = None
             last_point_sees_next = False
 
