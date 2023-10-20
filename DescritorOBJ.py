056bab522c88e9af904d8c7db7f418bedac0a61e
bnmfw <bernardoborgessandoval@gmail.com>
2023-10-20 18:14:04 -0300

    readme

--- DescritorOBJ.py
+++ DescritorOBJ.py
@@ -7,7 +7,7 @@ class DescritorOBJ:
     def __init__(self):
         pass
 
-    def save_objs(self,objetos):
+    def save_objs(self, objetos):
         '''
         objetos : dicionário de objetos da tela
 
@@ -16,79 +16,76 @@ class DescritorOBJ:
         if not os.path.exists("objetos"):
             os.makedirs("objetos")
         
-        arquivo_nome = os.path.join("objetos", "cena.obj")
-        arquivo = open(arquivo_nome, "w")
-        fila_para_escrita = list()
-        dic_verticies = dict()
-        contador_v = 1
-        for key in objetos:
-            objeto = objetos[key]
-            escrita_futura_coords = "x "
+        with open("objetos/cena.obj", "w") as arquivo:
+            fila_para_escrita = list()
+            dic_verticies = dict()
+            contador_v = 1
+            for objeto in objetos.values():
+                escrita_futura_coords = "x "
 
-            if type(objeto) == Curved2D:
-                coordenadas = []
-                for segmento in objeto.curvas:
-                    for ponto in segmento:
-                        if ponto not in coordenadas:
+                if type(objeto) == Curved2D:
+                    coordenadas = []
+                    for i, segmento in enumerate(objeto.curvas):
+                        if not i:
+                            coordenadas.append(segmento[0])
+                        for ponto in segmento[1:]:
                             coordenadas.append(ponto)
-                print(coordenadas)
-            elif type(objeto) == BSpline:
-                coordenadas = objeto.control_points
-            else:
-                coordenadas = objeto.coord_world
+                elif type(objeto) == BSpline: coordenadas = objeto.control_points
+                else: coordenadas = objeto.coord_world
 
 
-            escrita_v = ""
+                escrita_v = ""
 
-            if len(coordenadas) == 1:
-                if coordenadas not in dic_verticies:
-                    dic_verticies[coordenadas[0]] = contador_v
-                    escrita_v +="\n" + " v "+ coordenadas[0]
-                    contador_v += 1
-                    escrita_futura_coords = "p "+str(dic_verticies[coordenadas]) #TESTAR
-
-            else:
-
-                for coord in coordenadas:
-                    if coord not in dic_verticies:
-                        dic_verticies[coord] = contador_v
-                        escrita_v += "\n" + "v "+ str(coord[0]) + " " + str(coord[1]) + " " + "0"
+                if len(coordenadas) == 1:
+                    if coordenadas not in dic_verticies:
+                        dic_verticies[coordenadas[0]] = contador_v
+                        escrita_v +="\n" + " v "+ coordenadas[0]
                         contador_v += 1
-                    escrita_futura_coords += str(dic_verticies[coord]) + " " #TESTAR
+                        escrita_futura_coords = "p "+str(dic_verticies[coordenadas]) #TESTAR
 
-                if type(objeto) == Wireframe_filled:
-                    escrita_futura_coords="f"+ escrita_futura_coords[1:]
-                elif type(objeto) == Curved2D:
-                    escrita_futura_coords="b"+escrita_futura_coords[1:]
-                elif type(objeto) == BSpline:
-                    escrita_futura_coords="s"+escrita_futura_coords[1:]
                 else:
-                    escrita_futura_coords="l"+ escrita_futura_coords[1:]
 
-            
-            
-            escrita_cores = objeto.color.getRgb()
-            escrita_cores = "Kd " + str(escrita_cores[0]) + " " + str(escrita_cores[1]) + " " + str(escrita_cores[2])
-            fila_para_escrita.append(escrita_cores)
-            fila_para_escrita.append(escrita_futura_coords)
-            escrita_futura_nome = "o "+objeto.label
-            fila_para_escrita.append(escrita_futura_nome)
+                    for coord in coordenadas:
+                        if coord not in dic_verticies:
+                            dic_verticies[coord] = contador_v
+                            escrita_v += "\n" + "v "+ str(coord[0]) + " " + str(coord[1]) + " " + "0"
+                            contador_v += 1
+                        escrita_futura_coords += str(dic_verticies[coord]) + " " #TESTAR
 
-            arquivo.write(escrita_v) #isso definitivamente precisa
-            # print(escrita_v)
-           #arquivo.write("color "+str(objeto.color.getRgb())+"\n") #precisa mas escrito diferente
-        
-        count_fila = contador_v
-        while(fila_para_escrita):
-            temp = fila_para_escrita.pop()
-            arquivo.write("\n" + temp )
-            # print(temp)
-            count_fila +=1
+                    if type(objeto) == Wireframe_filled:
+                        escrita_futura_coords="f"+ escrita_futura_coords[1:]
+                    elif type(objeto) == Curved2D:
+                        escrita_futura_coords="b"+escrita_futura_coords[1:]
+                    elif type(objeto) == BSpline:
+                        escrita_futura_coords="s"+escrita_futura_coords[1:]
+                    else:
+                        escrita_futura_coords="l"+ escrita_futura_coords[1:]
+
+                
+                
+                escrita_cores = objeto.color.getRgb()
+                escrita_cores = "Kd " + str(escrita_cores[0]) + " " + str(escrita_cores[1]) + " " + str(escrita_cores[2])
+                fila_para_escrita.append(escrita_cores)
+                fila_para_escrita.append(escrita_futura_coords)
+                escrita_futura_nome = "o "+objeto.label
+                fila_para_escrita.append(escrita_futura_nome)
+
+                arquivo.write(escrita_v) #isso definitivamente precisa
+                # print(escrita_v)
+            #arquivo.write("color "+str(objeto.color.getRgb())+"\n") #precisa mas escrito diferente
+            
+            count_fila = contador_v
+            while(fila_para_escrita):
+                temp = fila_para_escrita.pop()
+                arquivo.write("\n" + temp )
+                # print(temp)
+                count_fila +=1
 
     def load_objs(self):
         with open("objetos/cena.obj", "r") as arquivo:
             vetores, objetos = {}, {}
             contador_vetores = 1
+            type_map = {"s": "BSpline", "f": "Polygon", "b": "Curved2D", "l": "Wireframe Open"}
             current_object = {"name": "", "points": None, "color": QtGui.QColor(255,0,0), "type": None}
             for linha in arquivo:
 
@@ -108,6 +105,8 @@ class DescritorOBJ:
                 # Indetifica uma cor RGB a ser lida
                 elif identficador == "Kd":
                     current_object["color"] = QtGui.QColor(int(info[1]),int(info[2]),int(info[3]))
+                    
+                    # Carrega o objeto atual e inicia um novo
                     objetos[current_object["name"]] = current_object
                     current_object = {"name": "", "points": None, "color": QtGui.QColor(255,0,0), "type": None}
                    
@@ -119,14 +118,10 @@ class DescritorOBJ:
                         if current_object["points"] is None: current_object["points"] = []
                         current_object["points"].append(ponto)
 
-                    if identficador == "s": current_object["type"] = "BSpline"
-                    elif identficador == "f": current_object["type"] = "Polygon"
-                    elif identficador == "b": current_object["type"] = "Curved2D"
-                    elif identficador == "l": 
-                        if current_object["points"][0] == current_object["points"][-1]:
-                            current_object["type"] = "Wireframe Closed"
-                        else:
-                            current_object["type"] = "Wireframe Open"
+                    # Salva o tipo correto
+                    current_object["type"] = type_map[identficador]
+                    if current_object["type"] == "Wireframe Open" and current_object["points"][0] == current_object["points"][-1]:
+                        current_object["type"] = "Wireframe Closed"
                              
         return objetos
 
--- README.md
+++ README.md
@@ -1,60 +1,115 @@
+
 # INE5420 - COMPUTAÇÃO GRÁFICA
+
+  
 
 <li><strong>NOVIDADES DO README ESTARÃO EM NEGRITO</strong></li>
 
+  
+
 <li><STRONG>Requisitos:</STRONG></li>
 
-		PyQt5
-		numpy
+  
+
+PyQt5
+
+numpy
+
+  
 
 <li><strong>Iniciando:</strong></li>
-	
-		make
+
+make
+
+  
 
 <li><strong>Criando um novo objeto:</strong></li>
 
- 1. Clique em "Novo objeto" no canto superior esquerdo da tela
- 2. Insira seu nome
- 3. Insira as coordenadas (Formato: (x0, y0) (x1, y1) (x2, y2)...)
- 4. Um código RGB pode ser inserido para definir a cor do objeto, caso não inserido o padrão é 255 0 0.
- 5. Uma check box pode ser selecionada pra fechar o poligono, unir o ponto final e inicial.
- 6. Uma checkbox pode ser selecionada para dar preenchimento ao poligono.
- 7. <strong>Uma checkbox pode ser selecionada para interpretar as coordenadas como pontos de controle de Bezier de uma curva, neste caso o número de pontos tem de seguir o padrão 4 + 3 * k onde k é um inteiro qualquer. O conjunto de curvas terá continuidade C(0), o ponto final de uma é o inicial de outra. Não selecione essa checkbox se selecionar outra.</strong>
+  
 
-<li><strong>Curvas:</strong></li>
- 
- - <strong>Todas as curvas renderizada sao curvas de Bezier.</strong>
- - <strong>A quantidade de pontos por curva é decidida dinâmicamente dado o zoom da window de forma que a curva sempre fique em qualidade razoável.</strong>
+1. Clique em "Novo objeto" no canto superior esquerdo da tela
+
+2. Insira seu nome
+
+3. Insira as coordenadas (Formato: (x0, y0) (x1, y1) (x2, y2)...)
+
+4. Um código RGB pode ser inserido para definir a cor do objeto, caso não inserido o padrão é 255 0 0.
+
+5. <strong> Um seletor permite escolher o tipo de objeto criado a partir das coordenadas. Obs: (Curved 2D tem de ter 1+3*k pontos e BSpline tem de ter 4 pontos ou mais </strong>
+
+  
+
+<li>Curvas:</li>
+
+-  A quantidade de pontos por curva é decidida dinâmicamente dado o zoom da window de forma que a curva sempre fique em qualidade razoável.
+
+  
 
 <li><strong>Transformacoes:</strong> Há um pad com as transformações na esquerda da interface.</li>
 
- - Translacao (Setas): Unidades de deslocamento no mundo (Considera coordenadas do mundo).
- - Escala (Quadrados): Proporção do aumento ou redução.
- - Rotação (Seta circular): Angulo em graus da rotação no sentido horario.
+  
+
+- Translacao (Setas): Unidades de deslocamento no mundo (Considera coordenadas do mundo).
+
+- Escala (Quadrados): Proporção do aumento ou redução.
+
+- Rotação (Seta circular): Angulo em graus da rotação no sentido horario.
+
+  
 
 <li><strong>Transformando Objetos:</strong></li>
 
- - Enquanto houver pelo menos um objeto selecionado as transformações serão aplicadas a esses objetos.
- - Objetos de arame selecionados ficam com a borda grossa.
- - Objetos preenchidos selecionados ficam com uma borda preta.
- - Se não houver nenhum objeto selecionado as transformações serão feitas na window.
- - Apenas objetos selecionados serão transformados (Eles ficam com a borda grossa).
- - Se existir um ponto central descrito eles usarão o ponto como centro de transformações.
+  
+
+- Enquanto houver pelo menos um objeto selecionado as transformações serão aplicadas a esses objetos.
+
+- Objetos de arame selecionados ficam com a borda grossa.
+
+- Objetos preenchidos selecionados ficam com uma borda preta.
+
+- Se não houver nenhum objeto selecionado as transformações serão feitas na window.
+
+- Apenas objetos selecionados serão transformados (Eles ficam com a borda grossa).
+
+- Se existir um ponto central descrito eles usarão o ponto como centro de transformações.
+
+  
 
 <li><strong>Ponto Central:</strong></li>
 
- - O Usuário pode inserir coordenadas de um ponto central nas caixas X: e Y:
- - Enquanto as coordenadas estiverem inseridas transformações de escala e rotação o usuarão como centro.
- - Caso não inserido objetos usam seus próprio centros como centro.
- - Transformações da window <strong>SEMPRE</strong> usam o centro da própria window.
+  
+
+- O Usuário pode inserir coordenadas de um ponto central nas caixas X: e Y:
+
+- Enquanto as coordenadas estiverem inseridas transformações de escala e rotação o usuarão como centro.
+
+- Caso não inserido objetos usam seus próprio centros como centro.
+
+- Transformações da window SEMPRE usam o centro da própria window.
+
+  
 
 <li><strong>Visão de Mundo:</strong></li>
 
- - A check box visão de mundo permite enxergar o mundo no sistema de coordenadas da viewport.
- - A própria window será renderizada no mundo como um retângulo preto de borda grossa.
+  
+
+- A check box visão de mundo permite enxergar o mundo no sistema de coordenadas da viewport.
+
+- A própria window será renderizada no mundo como um retângulo preto de borda grossa.
+
+  
 
 <li><strong>Clipping:</li></strong>
 
- - A tela toda é um canvas, o viewport é apenas um retângulo preto de fundo branco.
- - Há três opções de *clipping* para wireframes, A opção de clipping selecionada é utilizada como base do clipping de poligonos preenchidos.
- - <strong>Há também três opções de *clipping* para curvas, A opção de Completa faz o clipping como se as curvas fossem wireframes, assim todo segmento da curva é clippado individualmente. A opção parcial é a apresentada em sala, a curva é percorrida das extremidades para dentro e encerra quando uma borda é encontrada, esta opção pode fazer com que partes da curva que deveriam ser renderizadas não sejam</strong>
+  
+
+- A tela toda é um canvas, o viewport é apenas um retângulo preto de fundo branco.
+
+- Há três opções de *clipping* para wireframes, A opção de clipping selecionada é utilizada como base do clipping de poligonos preenchidos.
+
+-  Há também três opções de *clipping* para curvas, A opção de Completa faz o clipping como se as curvas fossem wireframes, assim todo segmento da curva é clippado individualmente. A opção parcial é a apresentada em sala, a curva é percorrida das extremidades para dentro e encerra quando uma borda é encontrada, esta opção pode fazer com que partes da curva que deveriam ser renderizadas não sejam
+- <strong> BSplines são clippadas como wireframes quando clippadas.</strong>
+
+<li><strong>Ajustar aos Objetos:</li></strong>
+
+- <strong> Um botão de Ajustas aos Objetos foi acrescentado, quando clicado ele ajusta a window para que todos os objetos estejam dentro dela e o mínimo de espaço excedente fique sobrando.</strong>
